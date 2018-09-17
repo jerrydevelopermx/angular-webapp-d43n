a08154ac80f3d504ca07fd39072270e96b6c5343
Gerardo Garcia <jerry@Gerardos-MacBook-Pro.local>
2018-09-17 08:06:06 -0500

    Added floating menu

--- app/scripts/components/common/menu.js
+++ app/scripts/components/common/menu.js
@@ -6,6 +6,53 @@
         bindings: {
           data: '<',
         },
-        templateUrl: 'views/common/menu.html'
+        templateUrl: 'views/common/menu.html',
+        controller: componentController
       })
+
+      function componentController($scope, $timeout, $mdSidenav){
+
+        var vm = this;
+        vm.toggleLeft = buildDelayedToggler('left');
+        vm.toggleRight = buildToggler('right');
+        vm.isOpenRight = function() {
+          return $mdSidenav('right').isOpen();
+        };
+
+        function debounce(func, wait, context) {
+          var timer;
+          return function debounced() {
+            var context = $scope,
+              args = Array.prototype.slice.call(arguments);
+            $timeout.cancel(timer);
+            timer = $timeout(function() {
+              timer = undefined;
+              func.apply(context, args);
+            }, wait || 10);
+          };
+        }
+
+        function buildDelayedToggler(navID) {
+          return debounce(function() {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav(navID)
+              .toggle()
+              .then(function() {
+                //$log.debug("toggle " + navID + " is done");
+              });
+          }, 200);
+        }
+
+        function buildToggler(navID) {
+          return function() {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav(navID)
+              .toggle()
+              .then(function() {
+              //  $log.debug("toggle " + navID + " is done");
+              });
+          }
+        }
+
+      }
 })();
